eaa7813e4ef0b02c6f432a8f46be682e5221500d
Ada <adachaparro3@gmail.com>
2022-08-07 02:34:05 -0700

    strecch goal update

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 
-//Globals
+/*
+Overall: search through api
+
+When: DOMContentLoaded
+
+Cause: Click search button
+
+Effect: retrieves searched query console.log for now, render after.
+
+*/
+
+// Globals
 function clearPage() {
   mainDiv().innerHTML = ""
 }
 
-//Nodes
+// let recipeData = [];
+
+// Nodes
 const mainDiv = () => document.getElementById('main');
 const favoritesPage = () => document.getElementById('favorites-list-link');
 const homePageLink = () => document.getElementById('homepage-link');
 const randomRecipePage = () => document.getElementById('random-recipe-link');
+const searchRecipesPage = () => document.getElementById('search-recipes-link');
 
-//Renderers
+// Renderers
 function renderHomePage() {
-  //Create HTML for Homepage
+  // Create HTML for Homepage
   clearPage();
 
   const h1 = document.createElement("h1");
   const p = document.createElement("p");
 
   h1.innerText = "Welcome To The Little Cookbook!"
-  p.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+  p.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 
   mainDiv().appendChild(h1);
   mainDiv().appendChild(p);
@@ -32,7 +46,7 @@ function renderFavoritesPage() {
   const ul = document.createElement("ul");
   const li = document.createElement("li");
 
-  h1.innerText = "Favorite Recipes"
+  h1.innerText = "Your Favorite Recipes"
   li.innerText = "This is a Favorited Recipe"
 
   ul.appendChild(li);
@@ -53,12 +67,37 @@ function renderRandomRecipePage() {
   mainDiv().appendChild(p);
 }
 
-//Event Listeners
+function renderSearchRecipesPage() {
+  clearPage();
+
+  const h1 = document.createElement("h1");
+  h1.innerText = "Search Our Database!"
+
+  const searchbarHTML = `
+  <form id="search-form"> 
+  <input type="text" id="search-query" placeholder="Search Recipes...">
+  <button>Search</button>
+  </form>
+  `
+
+  mainDiv().appendChild(h1);
+  mainDiv().insertAdjacentHTML("beforeend", searchbarHTML);
+
+  const searchQueryElement = () => document.getElementById('search-query');
+  const searchQuery = searchQueryElement.value;
+
+
+
+
+}
+
+// Events
 document.addEventListener("DOMContentLoaded", () => {
   renderHomePage();
   favoritesListEvent();
   homePageEvent();
   randomRecipeEvent();
+  searchRecipesEvent();
 })
 
 function favoritesListEvent() {
@@ -72,3 +111,8 @@ function homePageEvent() {
 function randomRecipeEvent() {
   randomRecipePage().addEventListener("click", renderRandomRecipePage);
 }
+
+function searchRecipesEvent() {
+  searchRecipesPage().addEventListener("click", renderSearchRecipesPage);
+}
+
